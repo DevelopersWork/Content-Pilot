a6cfdb0f298ebb87c00a19a573eca2c32d1e3433
Developers@Work <DevelopersWork@users.noreply.github.com>
2021-12-04 21:40:48 +0530

    Create YouTube.php

---
+++ src/Core/YouTube.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+ * @package DevWPContentAutopilot
+ */
+namespace Dev\WpContentAutopilot\Core;
+
+class YouTube {
+
+    public static $_key = "";
+
+    public static function search($search_string = null) {
+        
+    }
+
+}
