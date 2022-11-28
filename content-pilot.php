7d16a189afad33019d65524ec87bfa5da2d7453e
Developers@Work <DevelopersWork@users.noreply.github.com>
2022-11-28 21:38:15 +0530

    improved development env

--- content-pilot.php
+++ content-pilot.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name
  *
@@ -9,14 +10,14 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Content Pilot
- * Plugin URI:        https://example.com/plugin-name
+ * Plugin URI:        https://github.com/DevelopersWork/Content-Pilot
  * Description:       Crawls internet to create automated posts.
  * Version:           0.1.1
- * Requires at least: 5.2
- * Requires PHP:      7.2
+ * Requires at least: 6.1.1
+ * Requires PHP:      8.1
  * Author:            DevelopersWork
  * Author URI:        https://thedevelopers.work
- * Text Domain:       plugin-slug
+ * Text Domain:       dw-content-pilot
  * License:           GPL v2 or later
  * License URI:       https://raw.githubusercontent.com/DevelopersWork/Content-Pilot/release/master/license.txt
  * Update URI:        https://github.com/DevelopersWork/Content-Pilot/tags
@@ -25,74 +26,73 @@
 $error = false;
 
 // If Absolute Path is not defined no point in starting this script.
-if( ! defined('ABSPATH') ) $error = true;
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+if (!defined('ABSPATH')) $error = true;
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
 // Require once the Composer Autoload
-if ( file_exists(dirname(__FILE__).'/vendor/autoload.php' ) ){
-    require_once dirname(__FILE__).'/vendor/autoload.php';
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
 } else $error = true;
 
 use DW\ContentPilot\Main;
-use DW\ContentPilot\Lib\{ Activate, Deactivate };
+use DW\ContentPilot\Lib\{Activate, Deactivate};
 
 // If someone already using the prefix hell and heaven with them
-if( defined('DWContetPilotPrefix') ) $error = true;
+if (defined('DWContetPilotPrefix')) $error = true;
 else define('DWContetPilotPrefix', 'dw_cp');
 
-define( 'dw_cp_plugin_name', 'Content Pilot');
-define( 'dw_cp_plugin_version', '0.1.1');
-define( 'dw_cp_plugin_dir_path', plugin_dir_path(__FILE__));
-define( 'dw_cp_plugin_dir_url', plugin_dir_url(__FILE__));
-define( 'dw_cp_plugin_base_name', plugin_basename(__FILE__));
+define('dw_cp_plugin_name', 'Content Pilot');
+define('dw_cp_plugin_version', '0.1.1');
+define('dw_cp_plugin_dir_path', plugin_dir_path(__FILE__));
+define('dw_cp_plugin_dir_url', plugin_dir_url(__FILE__));
+define('dw_cp_plugin_base_name', plugin_basename(__FILE__));
 
-class DWContentPilot {
+class DWContentPilot
+{
 
     private $main;
 
-    public function __construct() {
+    public function __construct()
+    {
 
         $_activate = new Activate();
-        register_activation_hook( __FILE__, array($_activate, 'activate') );
+        register_activation_hook(__FILE__, array($_activate, 'activate'));
 
         $_deactivate = new Deactivate();
-        register_deactivation_hook( __FILE__, array($_deactivate, 'deactivate') );
+        register_deactivation_hook(__FILE__, array($_deactivate, 'deactivate'));
 
-        if ( !is_plugin_active( dw_cp_plugin_base_name ) ) {
+        if (!is_plugin_active(dw_cp_plugin_base_name)) {
             return;
         }
 
-        if(isset($_REQUEST[DWContetPilotPrefix.'_API'])) {
-            
+        if (isset($_REQUEST[DWContetPilotPrefix . '_API'])) {
         } else {
-            $this -> main = new Main();
-            add_action( 'plugins_loaded', array($this, 'plugins_loaded') );
+            $this->main = new Main();
+            add_action('plugins_loaded', array($this, 'plugins_loaded'));
         }
     }
 
-    public function plugins_loaded() {
+    public function plugins_loaded()
+    {
 
-        add_action( 'init', array($this -> main, 'init') );
+        add_action('init', array($this->main, 'init'));
 
-        add_action( 'wp_loaded', array($this, 'wp_loaded') );
-
+        add_action('wp_loaded', array($this, 'wp_loaded'));
     }
 
-    public function wp_loaded() {
-
+    public function wp_loaded()
+    {
     }
-
 }
 
-if ( class_exists('DWContentPilot') && $error == FALSE ) {
+if (class_exists('DWContentPilot') && $error == FALSE) {
 
     $classes = array(
-        'Google_Client' => Google_Client:: class,
-        'Google_Service_YouTube' => Google_Service_YouTube:: class,
-        'PHPHtmlParser' => PHPHtmlParser\Dom:: class
+        'Google_Client' => Google_Client::class,
+        'Google_Service_YouTube' => Google_Service_YouTube::class,
+        'PHPHtmlParser' => PHPHtmlParser\Dom::class
     );
-    define( 'dw_cp_classes', $classes);
+    define('dw_cp_classes', $classes);
 
     new DWContentPilot();
-
 }
---
+++ init.sh
@@ -0,0 +1,38 @@
+if [ "$ENV" != "development" ]
+then
+    exit 1
+fi
+
+###################################
+# Installing Node Version Manager #
+###################################
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+###########################
+# Installing PHP Composer #
+###########################
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --install-dir=/bin --filename=composer --quiet
+php -r "unlink('composer-setup.php');"
+
+# Restart terminal to use the installed commands
+source ~/.bashrc
+
+# Installs the provided node in .nvmrc
+nvm install
+
+# Installs node packages from package.json
+npm install
+
+# Installs php composer packages from composer.json
+php /bin/composer install
