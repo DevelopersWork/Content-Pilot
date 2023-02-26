e6c0c54b87fd4feaa6742bd12a49b7744c8a6a10
Developers@Work <DevelopersWork@users.noreply.github.com>
2023-02-26 23:14:58 +0530

    #48: PHPUnit Completed

---
+++ tests/credentials/sampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package DWContentPilot
+ */
+
+namespace DW\ContentPilot\Tests\Credentials;
+
+use PHPUnit\Framework\TestCase;
+
+class sampleTest extends TestCase
+{
+    public function testAddition()
+    {
+        $result = 1 + 2;
+        $this->assertEquals(3, $result);
+    }
+}
